520327311abb64da11d3c8cd9cd44847689dbdb5
daffa_rasyid <daffa.naufan@gmail.com>
2022-03-22 18:00:59 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <p>tes</p>
+    </div>
+  );
+}
+
+export default App;
